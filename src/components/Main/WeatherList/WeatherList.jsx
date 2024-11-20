6b9d6943aa6bd4c7ac9ace290724bb492febd16a
Victor Garritano <victor@getupgraded.com>
2024-11-20 15:11:21 +0100

    done

---
+++ src/components/Main/WeatherList/WeatherList.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const WeatherList = () => {
+  return <div>WeatherList</div>;
+};
+
+export default WeatherList;
+//api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
