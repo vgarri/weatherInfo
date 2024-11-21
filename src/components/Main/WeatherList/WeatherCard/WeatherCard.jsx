be633e18b9f42606cc5c20d177a41be926c26168
Victor Garritano <victor@getupgraded.com>
2024-11-21 12:53:13 +0100

    done

--- src/components/Main/WeatherList/WeatherCard/WeatherCard.jsx
+++ src/components/Main/WeatherList/WeatherCard/WeatherCard.jsx
@@ -3,10 +3,17 @@ import React from "react";
 const WeatherCard = (w) => {
 
   const {weather} = w
+  const icon = weather.weather[0].icon
   
   return (<>
  
     <article className="weather-card">
+      <div className="icon">
+      <img
+        src={`https://openweathermap.org/img/wn/${icon}@2x.png`}
+      
+      />
+      </div>
       <h3>Time: {weather.dt_txt.split(" ")[1].slice(0,5)}</h3>
       <h4>Description: {weather.weather[0].description}</h4>
       <h5>Date: {weather.dt_txt.slice(0,10)}</h5>
